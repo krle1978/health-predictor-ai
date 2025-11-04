27896d08aa2ac226a24e6f0dfd0a14b7973eef9c
krle1978 <krstic.rade@gmail.com>
2025-11-04 16:16:36 +0100

    Added dynamic port for Render

--- backend/app.py
+++ backend/app.py
@@ -161,6 +161,5 @@ def predict_stroke():
 
 # === MAIN ENTRY ===
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    print(f"🚀 Flask backend running on port {port}")
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
